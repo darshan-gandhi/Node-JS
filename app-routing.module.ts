5d22fa0dd97a0555d248b300954f21769f216c6e
Darshan Gandhi <44197177+darshan-gandhi@users.noreply.github.com>
2020-04-27 11:48:14 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StoreComponent } from './store/store.component';
+import { CartComponent } from './cart/cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'home',component:StoreComponent},
+  {path:'employee',component:StoreComponent},
+  {path:'dashboard',component:CartComponent},
+  // {path:'**',component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { StoreComponent } from './store/store.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StoreComponent,
+    CartComponent,
+    HomeComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

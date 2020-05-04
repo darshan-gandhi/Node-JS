66738d4aec0c75626ddf803f012ed587b26d4764
Darshan Gandhi <44197177+darshan-gandhi@users.noreply.github.com>
2020-05-04 16:24:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,87 @@
-const express = require('express')
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+Genre = require('./models/genre');
+Book = require('./models/books');
 
-const app = express()
+var app = express();
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
 
-const userController = require('./controller/user')
+app.get('/', function(req, res){
+    res.send("Please use /api/books or /api/genres");
+});
 
-const middleware = require('./utils/middleware')
+app.get('/api/genres',function(req,res){
+    Genre.getGenres(function(err, genres){
+        if(err){
+            throw err;
+        }
+        res.json(genres);
+    });
+});
 
-app.use(express.urlencoded({ extended: true }))
-app.use('/api/users', userController)
+app.get('/api/genres/:_id',function(req,res){
+    Genre.getGenreById(req.params._id, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
 
-app.use(middleware.unknownEndpoint)
-app.set('view engine','ejs')
+app.post('/api/genres',function(req,res){
+    var genre = req.body;
+    Genre.addGenre(genre, function(err, genres){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
 
-const server = app.listen(5000, (err) => {
-    if (err) console.error(err)
-    else {
-      console.log(`Server started at 5000`)
-      mongoose.connect('mongodb://127.0.0.1:27017/darshan', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-      }, (error) => {
-        if (error) console.error(error)
-        else console.log('Connected to mongodb uri')
-      })
-    }
-  })
+app.put('/api/genres/:_id',function(req,res){
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.updateGenre(id, genre, {}, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
+
+app.delete('/api/genres/:_id',function(req,res){
+    var id = req.params._id;
+    Genre.deleteGenre(id, function(err, genre){
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    });
+});
+
+
+app.get('/api/books',function(req,res){
+    Book.getBooks(function(err, books){
+        if(err){
+            throw err;
+        }
+        res.json(books);
+    });
+});
+
+app.get('/api/books/:_id',function(req,res){
+    Book.getBookById(req.params._id, function(err, book){
+        if(err){
+            throw err;
+        }
+        res.json(book);
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Listening on port 3000");
+});
